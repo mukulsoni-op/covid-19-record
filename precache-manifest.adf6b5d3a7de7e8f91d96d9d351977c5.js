c9ce1ba754b1550d3ebfa12f4a9ba784ff4c0154
mukulsoni-op <78497619+mukulsoni-op@users.noreply.github.com>
2021-04-27 22:25:10 +0530

    Updates

---
+++ precache-manifest.adf6b5d3a7de7e8f91d96d9d351977c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1060b32383416adc8fd89913d52f668",
+    "url": "/covid-19-record/index.html"
+  },
+  {
+    "revision": "59911688df344cb6a4f4",
+    "url": "/covid-19-record/static/css/main.0bea8a96.chunk.css"
+  },
+  {
+    "revision": "99fe0995163af71f7141",
+    "url": "/covid-19-record/static/js/2.e458720e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-record/static/js/2.e458720e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59911688df344cb6a4f4",
+    "url": "/covid-19-record/static/js/main.eb1b5e4d.chunk.js"
+  },
+  {
+    "revision": "031b6608f475799540de",
+    "url": "/covid-19-record/static/js/runtime-main.48b3bc8c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-record/static/media/image.d7265326.png"
+  }
+]);
